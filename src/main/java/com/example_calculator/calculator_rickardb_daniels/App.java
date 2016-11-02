1f52b7c140e3ea2433b7e87fa896dfe0eb182402
Rickard Berglund <rigurd@hotmail.com>
2016-11-02 09:06:29 +0100

    This is our first commit, started an empty Maven Project and pushed it to github.

---
+++ src/main/java/com/example_calculator/calculator_rickardb_daniels/App.java
@@ -0,0 +1,13 @@
+package com.example_calculator.calculator_rickardb_daniels;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
