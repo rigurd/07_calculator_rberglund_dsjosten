5485f4df99246dced39eaa1eb6830225c5253a35
Daniel Sjösten <dansjo_87@hotmail.com>
2016-11-02 11:49:19 +0100

    Starting to clean the code in the GUI

--- src/main/java/classes/Gui.java
+++ src/main/java/classes/Gui.java
@@ -19,18 +19,47 @@ import javax.swing.JTextField;
 */
 public class Gui extends JFrame implements ActionListener{
 
+	/**
+	 * Setting serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
 	private JFrame frmCalculator;
 	private JTextField field_input1;
 	
 	
 	// Instanciating the buttons
 	JButton btn7 = new JButton("7");
+	JButton btn8 = new JButton("8");
+	JButton btn9 = new JButton("9");
+	JButton btn4 = new JButton("4");
+	JButton btn5 = new JButton("5");
+	JButton btn6 = new JButton("6");
+	JButton btn1 = new JButton("1");
+	JButton btn2 = new JButton("2");
+	JButton btn3 = new JButton("3");
+	JButton btn0 = new JButton("0");
+	JButton btn_squirt = new JButton("Sqr");
+	JButton btn_pow = new JButton("Pow");
+	JButton btn_degrees = new JButton("Dgr");
+	JButton btn_radian = new JButton("Radian");
+	JButton btn_clear = new JButton("C");
+	JButton btn_addition = new JButton("+");
+	JButton btn_subtraction = new JButton("-");
+	JButton btn_division = new JButton("/");
+	JButton btn_multiply = new JButton("*");
+	JButton btn_random = new JButton("Random");
+	JButton btn_cos = new JButton("Cos");
+	
 
 	// Instanciating the labels
 	private JLabel labelNowPlaying = new JLabel("Now Playing:");
 	private JLabel labelSongPlaying = new JLabel("");
 	private JTextField field_input2;
 	private JTextField field_result;
+	
+
 	
 
 	/**
@@ -79,7 +108,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn7.setBounds(86, 112, 58, 49);
 		frmCalculator.getContentPane().add(btn7);
 		
-		JButton btn8 = new JButton("8");
+		
 		btn8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -87,35 +116,35 @@ public class Gui extends JFrame implements ActionListener{
 		btn8.setBounds(157, 112, 58, 49);
 		frmCalculator.getContentPane().add(btn8);
 		
-		JButton btn9 = new JButton("9");
+		
 		btn9.setBounds(227, 112, 58, 49);
 		frmCalculator.getContentPane().add(btn9);
 		
-		JButton btn4 = new JButton("4");
+		
 		btn4.setBounds(86, 174, 58, 49);
 		frmCalculator.getContentPane().add(btn4);
 		
-		JButton btn5 = new JButton("5");
+		
 		btn5.setBounds(157, 174, 58, 49);
 		frmCalculator.getContentPane().add(btn5);
 		
-		JButton btn76 = new JButton("6");
-		btn76.setBounds(227, 174, 58, 49);
-		frmCalculator.getContentPane().add(btn76);
 		
-		JButton btn1 = new JButton("1");
+		btn6.setBounds(227, 174, 58, 49);
+		frmCalculator.getContentPane().add(btn6);
+		
+		
 		btn1.setBounds(86, 230, 58, 49);
 		frmCalculator.getContentPane().add(btn1);
 		
-		JButton btn2 = new JButton("2");
+		
 		btn2.setBounds(157, 230, 58, 49);
 		frmCalculator.getContentPane().add(btn2);
 		
-		JButton btn3 = new JButton("3");
+		
 		btn3.setBounds(227, 230, 58, 49);
 		frmCalculator.getContentPane().add(btn3);
 		
-		JButton btn0 = new JButton("0");
+		
 		btn0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -123,7 +152,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn0.setBounds(157, 292, 58, 49);
 		frmCalculator.getContentPane().add(btn0);
 		
-		JButton btn_squirt = new JButton("Sqr");
+		
 		btn_squirt.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -131,7 +160,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn_squirt.setBounds(12, 112, 58, 49);
 		frmCalculator.getContentPane().add(btn_squirt);
 		
-		JButton btn_pow = new JButton("Pow");
+		
 		btn_pow.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -139,7 +168,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn_pow.setBounds(12, 174, 58, 49);
 		frmCalculator.getContentPane().add(btn_pow);
 		
-		JButton btn_degrees = new JButton("Dgr");
+		
 		btn_degrees.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -147,7 +176,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn_degrees.setBounds(12, 230, 58, 49);
 		frmCalculator.getContentPane().add(btn_degrees);
 		
-		JButton btn_radian = new JButton("Radian");
+		
 		btn_radian.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -155,7 +184,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn_radian.setBounds(12, 292, 71, 49);
 		frmCalculator.getContentPane().add(btn_radian);
 		
-		JButton btn_clear = new JButton("C");
+		
 		btn_clear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -163,7 +192,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn_clear.setBounds(227, 292, 58, 49);
 		frmCalculator.getContentPane().add(btn_clear);
 		
-		JButton btn_addition = new JButton("+");
+		
 		btn_addition.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -171,15 +200,15 @@ public class Gui extends JFrame implements ActionListener{
 		btn_addition.setBounds(313, 174, 58, 49);
 		frmCalculator.getContentPane().add(btn_addition);
 		
-		JButton btn_subtraction = new JButton("-");
+		
 		btn_subtraction.setBounds(313, 230, 58, 49);
 		frmCalculator.getContentPane().add(btn_subtraction);
 		
-		JButton btn_division = new JButton("/");
+
 		btn_division.setBounds(313, 292, 58, 49);
 		frmCalculator.getContentPane().add(btn_division);
 		
-		JButton btn_multiply = new JButton("*");
+		
 		btn_multiply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -187,7 +216,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn_multiply.setBounds(313, 112, 58, 49);
 		frmCalculator.getContentPane().add(btn_multiply);
 		
-		JButton btn_random = new JButton("Random");
+		
 		btn_random.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -195,7 +224,7 @@ public class Gui extends JFrame implements ActionListener{
 		btn_random.setBounds(12, 351, 79, 49);
 		frmCalculator.getContentPane().add(btn_random);
 		
-		JButton btn_cos = new JButton("Cos");
+		
 		btn_cos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
