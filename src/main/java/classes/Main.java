b546355e5cd06a769e51ac10226a07a49b45d6e1
Rickard Berglund <rigurd@hotmail.com>
2016-11-03 11:57:59 +0100

    Broke out Main from gui.java to main.java, also added a testclass for Gui, which is not complete yet!

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -3,9 +3,9 @@ package classes;
 public class Main {
 
 	public static void main(String[] args) {
+				Gui window = new Gui();
+				window.frmCalculator.setVisible(true);
 
-		BasicOperations bo = new BasicOperations();
-		
 	}
 
 }
---
+++ src/test/java/junit_tests/GuiTest.java
@@ -0,0 +1,19 @@
+package junit_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+import classes.Gui;
+
+public class GuiTest {
+
+	Gui gui = new Gui();
+	
+	@Test
+	public void testGui(){
+		
+	}
+
+}
