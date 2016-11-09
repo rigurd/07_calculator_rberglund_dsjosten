a6449f490d8a9fcd6eb17b662f5bd2a53f5f2940
Daniel Sjösten <dansjo_87@hotmail.com>
2016-11-09 09:08:38 +0100

    Added javadoc comments for the operationsclasses

--- src/main/java/classes/AdvancedOperations.java
+++ src/main/java/classes/AdvancedOperations.java
@@ -9,27 +9,58 @@ import interfaces.CalculatorAdvancedOperationsInterface;
  * 
  */
 public class AdvancedOperations extends BasicOperations implements CalculatorAdvancedOperationsInterface{
-
+	/**
+	 * SquareRoot - SquareRoot of one double.
+	 * @param firstNumber : the first number as a double
+	 * 
+	 * @return: the result of the SquareRoot.
+	 * */
 	public double SquareRoot(double firstNumber) {
 		return Math.sqrt(firstNumber);
 	}
-
+	/**
+	 * Pow - Pow of two doubles.
+	 * @param firstNumber : the first number as a double
+	 * @param secondNumber: the second number as a double
+	 * 
+	 * @return: the result of the Pow.
+	 * */
 	public double Pow(double firstNumber, double secondNumber) {
 		return Math.pow(firstNumber, secondNumber);
 	}
-
+	/**
+	 * toDegrees - toDegrees of one double.
+	 * @param firstNumber : the first number as a double
+	 * 
+	 * @return: the result of toDegrees.
+	 * */
 	public double toDegrees(double firstNumber) {
 		return Math.toDegrees(firstNumber);
 	}
-
+	/**
+	 * toRadians - toRadians of one double.
+	 * @param firstNumber : the first number as a double
+	 * 
+	 * @return: the result of toRadians.
+	 * */
 	public double toRadians(double firstNumber) {
 		return Math.toRadians(firstNumber);
 	}
-
+	/**
+	 * Random - Generates a random number that can be use for operations in the calculator.
+	 * 
+	 * @return: nothing.
+	 * */
 	public double Random() {
 		return Math.random();
 	}
-
+	/**
+	 * Modula - Modula of two doubles.
+	 * @param firstNumber : the first number as a double
+	 * @param secondNumber: the second number as a double
+	 * 
+	 * @return: the result of the Modula.
+	 * */
 	public double Modula(double firstNumber, double secondNumber){
 		return firstNumber % secondNumber;
 	}
--- src/main/java/classes/BasicOperations.java
+++ src/main/java/classes/BasicOperations.java
@@ -1,5 +1,7 @@
 package classes;
 
+import javax.swing.JOptionPane;
+
 import interfaces.CalculatorBasicOperationsInterface;
 
 /**
@@ -9,21 +11,52 @@ import interfaces.CalculatorBasicOperationsInterface;
  * 
  */
 public class BasicOperations implements CalculatorBasicOperationsInterface{
-
+	/**
+	 * Addition- Addition of two doubles.
+	 * @param firstNumber : the first number as a double
+	 * @param secondNumber: the second number as a double
+	 * 
+	 * @return: the result of the addition.
+	 * */
 	public double Addition(double firstNumber, double secondNumber) {
 		return (firstNumber)+(secondNumber);
 	}
-
+	/**
+	 * Subtraction - Subtraction of two doubles.
+	 * @param firstNumber : the first number as a double
+	 * @param secondNumber: the second number as a double
+	 * 
+	 * @return: the result of the subtraction.
+	 * */
 	public double Subtraction(double firstNumber, double secondNumber) {
 		return (firstNumber)-(secondNumber);
 	}
-
+	/**
+	 * Multiplication - Multiplication of two doubles.
+	 * @param firstNumber : the first number as a double
+	 * @param secondNumber: the second number as a double
+	 * 
+	 * @return: the result of the multiplication.
+	 * */
 	public double Multiplication(double firstNumber, double secondNumber) {
 		return (firstNumber)*(secondNumber);
 	}
-
+	/**
+	 * Division- Division of two doubles.
+	 * @param firstNumber : the first number as a double
+	 * @param secondNumber: the second number as a double
+	 * 
+	 * @return: the result of the division.
+	 * */
 	public double Division(double firstNumber, double secondNumber) {
-		return (firstNumber)/(secondNumber);
+		double result = firstNumber / secondNumber;	
+		
+		if (Double.isInfinite(result)) {
+			JOptionPane.showMessageDialog(null, "Error, division by zero!", "Error", JOptionPane.ERROR_MESSAGE);
+			return -0.12313432;
+		}else{
+		return firstNumber / secondNumber;
+		}
 	}
 
 
